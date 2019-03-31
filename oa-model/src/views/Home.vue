a9b809c62b4caead3a6e572e3076613efbfe5a47
lishang <lishang@cndatacom.com>
2019-03-31 16:43:38 +0800

    'ui引入'

---
+++ oa-model/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <el-containter>
+      <el-header>头部</el-header>
+      <el-containter>
+        <el-aside width="200px">
+          
+        </el-aside>
+        <el-main>中间</el-main>
+      </el-containter>
+    </el-containter>
+  </div>
+</template>
+
+<script>
+
+</script>
